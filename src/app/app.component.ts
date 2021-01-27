69bc927ab4da8ae136d7d6d584beeeaaccbdeb87
Adarshg315 <adarshg315@gmail.com>
2021-01-27 23:38:09 +0530

    done till form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hello-angular';
+    // courses = ['course1', 'course2', 'course3']
+    // viewMode = 'dgg'
+    // courses = [
+    //   {id: 1, name: "course1"},
+    //   {id: 2, name: "course2"},
+    //   {id: 3, name: "course3"},
+    // ]
+    // onAdd(){
+    //   this.courses.push({id: 4, name: "course4"})
+    // }
+    // onChange(course: { id: number; name: string; }){
+    //   let index = this.courses.indexOf(course)
+    //   this.courses.splice(index, 1)
+    // }
+    // onChange(course: { id: number; name: string; }){
+    //   course.name = "updated"
+    // }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './course/course.component';
 import { CoursesService } from './course/courses.service';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CoursesComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [CoursesService],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, CoursesComponent, ContactFormComponent],
+    imports: [BrowserModule, AppRoutingModule, FormsModule],
+    providers: [CoursesService],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
